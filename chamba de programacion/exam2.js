c0483adfd1700723e563311f524a6fd8ad62a32a
Jose Martin Ruiz Campos <a2221040020@alumnos.uat.edu.mx>
2024-10-28 16:16:55 -0600

    Add files via upload

---
+++ chamba de programacion/exam2.js
@@ -0,0 +1,13 @@
+const fetchpelismovi=async()=>{
+    const pelisGrid=document.getElementById(Pelis.grid);
+    for ( let i=1; i<= 20; i++){
+        const response=await fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=2/${i}`);
+        const Pelis = await response.json();
+        createmovie(Pelis);
+    }
+};
+const createmovie=(pelis) =>{
+    const PelisGrid=document.getElementById('pelis-grid');
+    const card=document.createElement('div');
+    card.classList.add('pelis-card');
+}
